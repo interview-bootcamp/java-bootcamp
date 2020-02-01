530a6332030b52bfd6fcb467440dcc97c74b10e8
interview-bootcamp <60537556+interview-bootcamp@users.noreply.github.com>
2020-02-02 01:21:01 +0530

    Update DemoClass.java

--- src/main/java/com/bootcamp/demo/DemoClass.java
+++ src/main/java/com/bootcamp/demo/DemoClass.java
@@ -4,6 +4,6 @@ public class DemoClass {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Boot Camp");
-        System.out.println("Hi I am Nayan");
+        System.out.println("Hi I am Nayan kumar");
     }
 }
