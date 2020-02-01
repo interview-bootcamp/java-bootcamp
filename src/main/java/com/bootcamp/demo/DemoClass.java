6bf22b206a7aefb5561af639458e910097d2b993
nayanjha007 <58367927+nayanjha007@users.noreply.github.com>
2020-02-01 15:16:37 +0530

    Nayan: Added sysout in DemoClass

--- src/main/java/com/bootcamp/demo/DemoClass.java
+++ src/main/java/com/bootcamp/demo/DemoClass.java
@@ -2,6 +2,8 @@ package com.bootcamp.demo;
 
 public class DemoClass {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Boot Camp");
+        System.out.println("Hi I am Nayan");
     }
 }
